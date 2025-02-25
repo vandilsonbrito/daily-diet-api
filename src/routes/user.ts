1ab0e79ffe096f9167a903191a53d52e8e6d5dad
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-02-25 15:45:07 -0300

    feat: adds login route and adds existing user verification on register

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,6 +4,26 @@ import { randomUUID } from "node:crypto";
 import { z } from "zod";
 
 export async function userRoutes(app: FastifyInstance) {
+    app.post('/login', async (req, reply) => {
+
+        const getUserBodySchema = z.object({
+            email: z.string().email(),
+            password: z.string().min(5),
+        })
+
+        const { email, password } = getUserBodySchema.parse(req.body); 
+
+        const user = await knex('users').where({ email }).first();
+        if(!user) {
+            return reply.status(404).send('User not found')
+        }
+        else if (user.password !== password) {
+            return reply.status(401).send('Invalid credentials')
+        }
+
+        return reply.status(200).send('User successfully logged in')
+    })
+
     app.post('/register', async (req, reply) => {
 
         const createUserBodySchema = z.object({
@@ -13,6 +33,11 @@ export async function userRoutes(app: FastifyInstance) {
         })
         
         const { name, email, password } = createUserBodySchema.parse(req.body);
+
+        const existingUser = await knex('users').where({ email: email }).first();
+        if(existingUser) {
+            return reply.status(409).send('Email already in use')
+        }
 
         await knex('users').insert({
             id: randomUUID(),
