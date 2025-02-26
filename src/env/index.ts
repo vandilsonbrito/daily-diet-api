e096ddafb19ee7960c19b9d0e080f81d0b6e0ca8
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-02-26 10:09:44 -0300

    feat: adds node-env verification and secret-key on env schema

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,11 +1,19 @@
+import { config } from 'dotenv';
 import 'dotenv/config';
 import z from 'zod';
+
+if(process.env.NODE_ENV === "test") {
+    config({ path: ".env.test" })
+} else {
+    config()
+}
 
 const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
     DATABASE_URL: z.string(),
     DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
     PORT: z.coerce.number().default(3333),
+    SECRET_KEY: z.string(),
 });
 
 const _env = envSchema.safeParse(process.env);
